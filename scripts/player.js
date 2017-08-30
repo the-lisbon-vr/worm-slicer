b208a42f284a68392dd8ead68a686d0336e21eae
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-08-30 13:41:46 +0100

    black screen if player sticks head outside of corridor

--- scripts/player.js
+++ scripts/player.js
@@ -5,7 +5,19 @@ AFRAME.registerComponent('player', {
 
     cameraEl.addEventListener('componentchanged', function (evt) {
       if (evt.detail.name !== 'position') { return; }
-      // console.log(evt.detail.newData);
+
+      boundaryX = Math.abs(evt.detail.newData.x);
+      boundaryZ = Math.abs(evt.detail.newData.z);
+
+      // show all black if player goes outside the corridor:
+      if (boundaryX >= 5 || boundaryZ >= 5){
+        console.log("outside x: " + evt.detail.newData.x);
+        console.log("outside z: " + evt.detail.newData.z);
+        headCollider.setAttribute('scale', '1 1 -1');
+      }
+      else {
+        headCollider.setAttribute('scale', '1 1 1');
+      }
     });
 
     headCollider.addEventListener('collide', function (e) {
