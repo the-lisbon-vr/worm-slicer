aa43d79ae99e0b5d17341253559c1610259d9d60
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-09-01 16:41:36 +0100

    require all assets through index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+// A-frame modules:
 require('aframe');
 require('aframe-physics-system');
-// require('aframe-extras');
 var extras = require('aframe-extras');
 extras.registerAll();
+require('aframe-teleport-controls');
+
+// Assets:
+require('../scripts/scene.js');
+require('../scripts/worm.js');
+require('../scripts/player.js');
